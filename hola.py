20a0d940245f038f59935377a8ca87ecb858348e
yeinermelo2301 <ymelo78@uan.edu.co>
2020-09-27 22:20:42 -0500

    se agrega --yeiner como argumento del programa

--- hola.py
+++ hola.py
@@ -1,3 +1,8 @@
-def say_hello ( name = "World" ):
-print ( "Hello " , name, "!" )
-say_hello()
+import argparse
+def say_hello ( yeiner = "World" ):
+print ( "Hello " , yeiner, "!" )
+parser = argparse.ArgumentParser( description = 'Say hello.' )
+parser.add_argument( '--yeiner' , type = str , required = False ,
+metavar = 'yeiner' , help = 'your yeiner' , default = 'World' )
+args = parser.parse_args()
+say_hello(args.yeiner)
